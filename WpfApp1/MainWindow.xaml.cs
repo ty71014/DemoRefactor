a6edb0afd95e8616aa8f00f29e9468513b50930b
Chung <ty71014@gmail.com>
2023-02-08 23:50:19 +0800

    [重構] 修改SynctTme001命名，並renameTimer的Tick

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ namespace WpfApp1
         private readonly ModbusClient plc1 = new ModbusClient();
         private readonly ModbusClient plc2 = new ModbusClient("192.168.50.11", 502);
 
-        private readonly DispatcherTimer SynctTme001 = new DispatcherTimer();
+        private readonly DispatcherTimer ReadFromPLCTimer = new DispatcherTimer();
         private readonly Timer UpdateTimeTimer = new Timer();
 
         private int W01, W02;
@@ -46,6 +46,89 @@ namespace WpfApp1
             DefiniteTimeTimer.Interval = 20000;
             DefiniteTimeTimer.Elapsed += OnDefiniteTimeElapsed;
             DefiniteTimeTimer.Start();
+
+            ReadFromPLCTimer.Interval = TimeSpan.FromSeconds(0.5);
+            ReadFromPLCTimer.Start();
+            ReadFromPLCTimer.Tick += ReadFromPLCTimer_Tick; ;
+        }
+
+        private void ReadFromPLCTimer_Tick(object sender, EventArgs e)
+        {
+             //IP01
+            if (ReadStrat01.Text == string.Empty && plc1.Connected)
+            {
+                ClearTextBox1To8();
+                StateReadSTR01.Text = "IP01缺少起始: " + DateTime.Now;
+            }
+            else if (ReadStrat01.Text != string.Empty && plc1.Connected)
+            {
+                try
+                {
+                    var Read001 = plc1.ReadHoldingRegisters(Convert.ToInt32(ReadStrat01.Text), 8);
+
+                    textBox01.Text = Read001[0].ToString();
+                    textBox02.Text = Read001[1].ToString();
+                    textBox03.Text = Read001[2].ToString();
+                    textBox04.Text = Read001[3].ToString();
+                    textBox05.Text = Read001[4].ToString();
+                    textBox06.Text = Read001[5].ToString();
+                    textBox07.Text = Read001[6].ToString();
+                    textBox08.Text = Read001[7].ToString();
+                    Addr00.Content = int.Parse(ReadStrat01.Text) + 0;
+                    Addr01.Content = int.Parse(ReadStrat01.Text) + 1;
+                    Addr02.Content = int.Parse(ReadStrat01.Text) + 2;
+                    Addr03.Content = int.Parse(ReadStrat01.Text) + 3;
+                    Addr04.Content = int.Parse(ReadStrat01.Text) + 4;
+                    Addr05.Content = int.Parse(ReadStrat01.Text) + 5;
+                    Addr06.Content = int.Parse(ReadStrat01.Text) + 6;
+                    Addr07.Content = int.Parse(ReadStrat01.Text) + 7;
+                    StateReadSTR01.Text = "IP01順利讀取: " + DateTime.Now;
+                }
+                catch (Exception)
+                {
+                    StateReadSTR01.Text = "IP01起始位置內容格式不對或不正常通訊: " + DateTime.Now;
+                    ClearTextBox1To8();
+                }
+            }
+
+
+            //IP02
+            if (ReadStrat02.Text == string.Empty && plc2.Connected)
+            {
+                ClearTextBox11To18();
+
+                StateReadSTR02.Text = "IP01缺少起始: " + DateTime.Now;
+            }
+            else if (ReadStrat02.Text != string.Empty && plc2.Connected)
+            {
+                try
+                {
+                    var Read002 = plc2.ReadHoldingRegisters(Convert.ToInt32(ReadStrat02.Text), 8);
+
+                    textBox11.Text = Read002[0].ToString();
+                    textBox12.Text = Read002[1].ToString();
+                    textBox13.Text = Read002[2].ToString();
+                    textBox14.Text = Read002[3].ToString();
+                    textBox15.Text = Read002[4].ToString();
+                    textBox16.Text = Read002[5].ToString();
+                    textBox17.Text = Read002[6].ToString();
+                    textBox18.Text = Read002[7].ToString();
+                    Addr10.Content = int.Parse(ReadStrat02.Text) + 0;
+                    Addr11.Content = int.Parse(ReadStrat02.Text) + 1;
+                    Addr12.Content = int.Parse(ReadStrat02.Text) + 2;
+                    Addr13.Content = int.Parse(ReadStrat02.Text) + 3;
+                    Addr14.Content = int.Parse(ReadStrat02.Text) + 4;
+                    Addr15.Content = int.Parse(ReadStrat02.Text) + 5;
+                    Addr16.Content = int.Parse(ReadStrat02.Text) + 6;
+                    Addr17.Content = int.Parse(ReadStrat02.Text) + 7;
+                    StateReadSTR02.Text = "IP02順利讀取: " + DateTime.Now;
+                }
+                catch (Exception)
+                {
+                    StateReadSTR02.Text = "IP02起始位置內容格式不對或不正常通訊: " + DateTime.Now;
+                    ClearTextBox11To18();
+                }
+            }
         }
 
         //通訊條件
@@ -63,10 +146,6 @@ namespace WpfApp1
 
                     if (plc1.Connected)
                     {
-                        SynctTme001.Interval = TimeSpan.FromSeconds(0.5);
-                        SynctTme001.Start();
-                        SynctTme001.Tick += syncTime001;
-
 
                         W01 = 0;
                         StateReadW01.Text = W01.ToString();
@@ -92,11 +171,6 @@ namespace WpfApp1
                     plc2.Available(3000);
                     if (plc2.Connected)
                     {
-                        SynctTme001.Interval = TimeSpan.FromSeconds(0.5);
-                        SynctTme001.Start();
-                        SynctTme001.Tick += syncTime001;
-
-
                         W02 = 0;
                         StateReadW02.Text = W02.ToString();
                         WriteToPLC2();
@@ -176,86 +250,7 @@ namespace WpfApp1
                 textBoxT_MiSec.Text = DateTime.Now.Millisecond.ToString();
             }));
         }
-
-        //顯示讀取
-        private void syncTime001(object sender, EventArgs e)
-        {
-            //IP01
-            if (ReadStrat01.Text == string.Empty && plc1.Connected)
-            {
-                ClearTextBox1To8();
-                StateReadSTR01.Text = "IP01缺少起始: " + DateTime.Now;
-            }
-            else if (ReadStrat01.Text != string.Empty && plc1.Connected)
-            {
-                try
-                {
-                    var Read001 = plc1.ReadHoldingRegisters(Convert.ToInt32(ReadStrat01.Text), 8);
-
-                    textBox01.Text = Read001[0].ToString();
-                    textBox02.Text = Read001[1].ToString();
-                    textBox03.Text = Read001[2].ToString();
-                    textBox04.Text = Read001[3].ToString();
-                    textBox05.Text = Read001[4].ToString();
-                    textBox06.Text = Read001[5].ToString();
-                    textBox07.Text = Read001[6].ToString();
-                    textBox08.Text = Read001[7].ToString();
-                    Addr00.Content = int.Parse(ReadStrat01.Text) + 0;
-                    Addr01.Content = int.Parse(ReadStrat01.Text) + 1;
-                    Addr02.Content = int.Parse(ReadStrat01.Text) + 2;
-                    Addr03.Content = int.Parse(ReadStrat01.Text) + 3;
-                    Addr04.Content = int.Parse(ReadStrat01.Text) + 4;
-                    Addr05.Content = int.Parse(ReadStrat01.Text) + 5;
-                    Addr06.Content = int.Parse(ReadStrat01.Text) + 6;
-                    Addr07.Content = int.Parse(ReadStrat01.Text) + 7;
-                    StateReadSTR01.Text = "IP01順利讀取: " + DateTime.Now;
-                }
-                catch (Exception)
-                {
-                    StateReadSTR01.Text = "IP01起始位置內容格式不對或不正常通訊: " + DateTime.Now;
-                    ClearTextBox1To8();
-                }
-            }
-
-
-            //IP02
-            if (ReadStrat02.Text == string.Empty && plc2.Connected)
-            {
-                ClearTextBox11To18();
-
-                StateReadSTR02.Text = "IP01缺少起始: " + DateTime.Now;
-            }
-            else if (ReadStrat02.Text != string.Empty && plc2.Connected)
-            {
-                try
-                {
-                    var Read002 = plc2.ReadHoldingRegisters(Convert.ToInt32(ReadStrat02.Text), 8);
-
-                    textBox11.Text = Read002[0].ToString();
-                    textBox12.Text = Read002[1].ToString();
-                    textBox13.Text = Read002[2].ToString();
-                    textBox14.Text = Read002[3].ToString();
-                    textBox15.Text = Read002[4].ToString();
-                    textBox16.Text = Read002[5].ToString();
-                    textBox17.Text = Read002[6].ToString();
-                    textBox18.Text = Read002[7].ToString();
-                    Addr10.Content = int.Parse(ReadStrat02.Text) + 0;
-                    Addr11.Content = int.Parse(ReadStrat02.Text) + 1;
-                    Addr12.Content = int.Parse(ReadStrat02.Text) + 2;
-                    Addr13.Content = int.Parse(ReadStrat02.Text) + 3;
-                    Addr14.Content = int.Parse(ReadStrat02.Text) + 4;
-                    Addr15.Content = int.Parse(ReadStrat02.Text) + 5;
-                    Addr16.Content = int.Parse(ReadStrat02.Text) + 6;
-                    Addr17.Content = int.Parse(ReadStrat02.Text) + 7;
-                    StateReadSTR02.Text = "IP02順利讀取: " + DateTime.Now;
-                }
-                catch (Exception)
-                {
-                    StateReadSTR02.Text = "IP02起始位置內容格式不對或不正常通訊: " + DateTime.Now;
-                    ClearTextBox11To18();
-                }
-            }
-        }
+        
 
         private void ClearTextBox11To18()
         {
@@ -319,7 +314,7 @@ namespace WpfApp1
                 StateReadSTR01.Text = "已斷線: " + DateTime.Now;
                 plc2.Disconnect();
                 StateReadSTR02.Text = "已斷線: " + DateTime.Now;
-                SynctTme001.Stop();
+                ReadFromPLCTimer.Stop();
             }
 
 
